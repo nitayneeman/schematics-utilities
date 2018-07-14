7e93b9e12a5668287245e6930f5fe1331a0553b5
nitayneeman <nitayneeman@users.noreply.github.com>
2018-07-14 13:19:46 +0300

    Updated docs scripts

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -8,6 +8,9 @@ npm run clean:docs
 # Building the docs
 npm run build:docs
 
+# Removing the local branch
+git branch -D gh-pages
+
 # Adding changes to git
 git add .
 
@@ -17,6 +20,3 @@ git commit -m "$msg"
 
 # Pushing changes
 git subtree push --prefix docs origin gh-pages
-
-# Removing the local branch
-git branch -D gh-pages
