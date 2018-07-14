7b1abe6474fa36f41cc81869c56fd02d4c232211
nitayneeman <nitayneeman@users.noreply.github.com>
2018-07-14 12:45:43 +0300

    Added script for publishing the docs

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -2,6 +2,9 @@
 
 echo -e "\033[0;32mDeploying updates to gh-pages...\033[0m"
 
+# Set up a worktree in directory docs checked out on branch gh-pages
+git worktree add docs gh-pages
+
 # Navigate to "docs" directory
 cd docs
 
@@ -13,7 +16,4 @@ msg="Deployment process - `date`"
 git commit -m "$msg"
 
 # Pushing changes
-git subtree push --prefix ./docs origin gh-pages
-
-# Navigate to parent directory
-cd ..
+git push origin gh-pages
