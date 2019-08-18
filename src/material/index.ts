cad17f6215ed07e6642c79c72400f992bef86760
nitayneeman <nitayneeman@users.noreply.github.com>
2019-08-18 22:02:10 +0300

    refactor: remove redundant file

--- src/material/index.ts
+++ src/material/index.ts
@@ -2,6 +2,5 @@ export * from './ast';
 export * from './build-component';
 export * from './get-project';
 export * from './html-head-element';
-export * from './lib-versions';
 export * from './package';
 export * from './testing';
