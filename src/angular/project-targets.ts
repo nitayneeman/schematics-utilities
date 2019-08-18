2311ca23a4ce81872f7abaf59f57fc68e6b4ac3d
nitayneeman <nitayneeman@users.noreply.github.com>
2019-08-18 20:44:18 +0300

    refactor: migrate to use the original `project-targets` file

--- src/angular/project-targets.ts
+++ src/angular/project-targets.ts
@@ -1,20 +1,3 @@
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
+import { getProjectTargets } from '@schematics/angular/utility/project-targets';
 
-import { experimental } from '@angular-devkit/core';
-
-export function getProjectTargets(
-  project: experimental.workspace.WorkspaceProject
-): experimental.workspace.WorkspaceTool {
-  const projectTargets = (<any>project).targets || project.architect;
-  if (!projectTargets) {
-    throw new Error('Project targets not found.');
-  }
-
-  return projectTargets;
-}
+export { getProjectTargets };
