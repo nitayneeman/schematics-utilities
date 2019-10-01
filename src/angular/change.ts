58e3aee7c5ba581ff4238d9972a76d506e3b696c
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-01 18:04:56 +0300

    refactor: migrate to use the original code

--- src/angular/change.ts
+++ src/angular/change.ts
@@ -1,10 +1,32 @@
 import {
-  Host,
-  Change,
-  NoopChange,
-  InsertChange,
-  RemoveChange,
-  ReplaceChange
+  Host as originalHost,
+  Change as originalChange,
+  NoopChange as originalNoopChange,
+  InsertChange as originalInsertChange,
+  RemoveChange as originalRemoveChange,
+  ReplaceChange as originalReplaceChange
 } from '@schematics/angular/utility/change';
 
-export { Host, Change, NoopChange, InsertChange, RemoveChange, ReplaceChange };
+export interface Host extends originalHost {}
+
+export interface Change extends originalChange {}
+
+/**
+ * An operation that does nothing.
+ */
+export class NoopChange extends originalNoopChange {}
+
+/**
+ * Will add text to the source code.
+ */
+export class InsertChange extends originalInsertChange {}
+
+/**
+ * Will remove text from the source code.
+ */
+export class RemoveChange extends originalRemoveChange {}
+
+/**
+ * Will replace text from the source code.
+ */
+export class ReplaceChange extends originalReplaceChange {}
