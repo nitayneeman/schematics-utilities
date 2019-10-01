904b621fda61d805e58e338b2abf7ab44266a84c
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-01 20:44:21 +0300

    refactor: migrate to use the original code

--- src/material/ast.ts
+++ src/material/ast.ts
@@ -1,6 +1,38 @@
-import { getSourceFile, addModuleImportToRootModule, addModuleImportToModule } from '@angular/cdk/schematics';
+import { Tree } from '@angular-devkit/schematics';
+import { Schema as ComponentOptions } from '@schematics/angular/component/schema';
+import {
+  getSourceFile as originalGetSourceFile,
+  addModuleImportToRootModule as originalAddModuleImportToRootModule,
+  addModuleImportToModule as originalAddModuleImportToModule,
+  findModuleFromOptions as originalFindModuleFromOptions,
+  typescript
+} from '@angular/cdk/schematics';
 
-export { getSourceFile, addModuleImportToRootModule, addModuleImportToModule };
+/** Reads file given path and returns TypeScript source file. */
+export function getSourceFile(host: Tree, path: string): typescript.SourceFile {
+  return originalGetSourceFile(host, path);
+}
+
+/** Import and add module to root app module. */
+export function addModuleImportToRootModule(host: Tree, moduleName: string, src: string, project: WorkspaceProject) {
+  return originalAddModuleImportToRootModule(host, moduleName, src, project);
+}
+
+/**
+ * Import and add module to specific module path.
+ * @param host the tree we are updating
+ * @param modulePath src location of the module to import
+ * @param moduleName name of module to import
+ * @param src src location to import
+ */
+export function addModuleImportToModule(host: Tree, modulePath: string, moduleName: string, src: string) {
+  return originalAddModuleImportToModule(host, modulePath, moduleName, src);
+}
+
+/** Wraps the internal find module from options with undefined path handling  */
+export function findModuleFromOptions(host: Tree, options: ComponentOptions): string | undefined {
+  return originalFindModuleFromOptions(host, options);
+}
 
 // TODO: Remove after finishing the migration completely
 export function getIndexHtmlPath(): any {}
