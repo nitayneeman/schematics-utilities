4d7175187ac5cd8440e3accfae408bd835c6f232
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-01 19:48:45 +0300

    refactor: migrate to use the original code

--- src/angular/lint-fix.ts
+++ src/angular/lint-fix.ts
@@ -1,3 +1,6 @@
-import { applyLintFix } from '@schematics/angular/utility/lint-fix';
+import { Rule } from '@angular-devkit/schematics';
+import { applyLintFix as originalApplyLintFix } from '@schematics/angular/utility/lint-fix';
 
-export { applyLintFix };
+export function applyLintFix(path = '/'): Rule {
+  return originalApplyLintFix(path);
+}
