6c1a1980f80c4020a9884509a05518dc5d1f810b
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-01 19:11:08 +0300

    refactor: migrate to use the original code

--- src/angular/dependencies.ts
+++ src/angular/dependencies.ts
@@ -1,8 +1,28 @@
+import { Tree } from '@angular-devkit/schematics';
 import {
-  NodeDependencyType,
-  NodeDependency,
-  addPackageJsonDependency,
-  getPackageJsonDependency
+  NodeDependency as OriginalNodeDependency,
+  addPackageJsonDependency as originalAddPackageJsonDependency,
+  getPackageJsonDependency as originalGetPackageJsonDependency,
+  removePackageJsonDependency as originalRemovePackageJsonDependency
 } from '@schematics/angular/utility/dependencies';
 
-export { NodeDependencyType, NodeDependency, addPackageJsonDependency, getPackageJsonDependency };
+export enum NodeDependencyType {
+  Default = 'dependencies',
+  Dev = 'devDependencies',
+  Peer = 'peerDependencies',
+  Optional = 'optionalDependencies'
+}
+
+export interface NodeDependency extends OriginalNodeDependency {}
+
+export function addPackageJsonDependency(tree: Tree, dependency: NodeDependency): void {
+  return originalAddPackageJsonDependency(tree, dependency);
+}
+
+export function removePackageJsonDependency(tree: Tree, name: string): void {
+  return originalRemovePackageJsonDependency(tree, name);
+}
+
+export function getPackageJsonDependency(tree: Tree, name: string): NodeDependency | null {
+  return originalGetPackageJsonDependency(tree, name);
+}
