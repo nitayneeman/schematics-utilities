6f78096f9c03730920a40e113a512a59e8ad663a
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-01 21:00:49 +0300

    refactor: migrate to use the original code

--- src/material/package-config.ts
+++ src/material/package-config.ts
@@ -1,3 +1,7 @@
-import { addPackageToPackageJson } from '@angular/cdk/schematics/ng-add/package-config';
+import { Tree } from '@angular-devkit/schematics';
+import { addPackageToPackageJson as originalAddPackageToPackageJson } from '@angular/cdk/schematics/ng-add/package-config';
 
-export { addPackageToPackageJson };
+/** Adds a package to the package.json in the given host tree. */
+export function addPackageToPackageJson(host: Tree, pkg: string, version: string): Tree {
+  return originalAddPackageToPackageJson(host, pkg, version);
+}
