55fe66499a3c505cd87786442bbec3012543f91c
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-01 18:35:30 +0300

    refactor: migrate to use the original code

--- src/angular/change.ts
+++ src/angular/change.ts
@@ -1,32 +1,32 @@
 import {
-  Host as originalHost,
-  Change as originalChange,
-  NoopChange as originalNoopChange,
-  InsertChange as originalInsertChange,
-  RemoveChange as originalRemoveChange,
-  ReplaceChange as originalReplaceChange
+  Host as OriginalHost,
+  Change as OriginalChange,
+  NoopChange as OriginalNoopChange,
+  InsertChange as OriginalInsertChange,
+  RemoveChange as OriginalRemoveChange,
+  ReplaceChange as OriginalReplaceChange
 } from '@schematics/angular/utility/change';
 
-export interface Host extends originalHost {}
+export interface Host extends OriginalHost {}
 
-export interface Change extends originalChange {}
+export interface Change extends OriginalChange {}
 
 /**
  * An operation that does nothing.
  */
-export class NoopChange extends originalNoopChange {}
+export class NoopChange extends OriginalNoopChange {}
 
 /**
  * Will add text to the source code.
  */
-export class InsertChange extends originalInsertChange {}
+export class InsertChange extends OriginalInsertChange {}
 
 /**
  * Will remove text from the source code.
  */
-export class RemoveChange extends originalRemoveChange {}
+export class RemoveChange extends OriginalRemoveChange {}
 
 /**
  * Will replace text from the source code.
  */
-export class ReplaceChange extends originalReplaceChange {}
+export class ReplaceChange extends OriginalReplaceChange {}
--- src/angular/config.ts
+++ src/angular/config.ts
@@ -1,26 +1,68 @@
+import { Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
 import {
-  AppConfig,
-  CliConfig,
-  getWorkspacePath,
-  getWorkspace,
-  addProjectToWorkspace,
-  configPath,
-  getConfig,
-  getAppFromConfig
+  AppConfig as OriginalAppConfig,
+  CliConfig as OriginalCliConfig,
+  getWorkspacePath as originalGetWorkspacePath,
+  getWorkspace as originalGetWorkspace,
+  addProjectToWorkspace as originalAddProjectToWorkspace,
+  updateWorkspace as originalUpdateWorkspace,
+  configPath as originalConfigPath,
+  getConfig as originalGetConfig,
+  getAppFromConfig as originalGetAppFromConfig
 } from '@schematics/angular/utility/config';
-import { experimental } from '@angular-devkit/core';
 
-export {
-  AppConfig,
-  CliConfig,
-  getWorkspacePath,
-  getWorkspace,
-  addProjectToWorkspace,
-  configPath,
-  getConfig,
-  getAppFromConfig
-};
+import { ProjectType, WorkspaceProject, WorkspaceSchema } from './workspace-models';
+
+// The interfaces below are generated from the Angular CLI configuration schema
+// https://github.com/angular/angular-cli/blob/master/packages/@angular/cli/lib/config/schema.json
+export interface AppConfig extends OriginalAppConfig {}
+
+export interface CliConfig extends OriginalCliConfig {}
+
+export function getWorkspacePath(host: Tree): string {
+  return originalGetWorkspacePath(host);
+}
+
+export function getWorkspace(host: Tree): WorkspaceSchema {
+  return originalGetWorkspace(host);
+}
+
+export function addProjectToWorkspace<TProjectType extends ProjectType = ProjectType.Application>(
+  workspace: WorkspaceSchema,
+  name: string,
+  project: WorkspaceProject<TProjectType>
+): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    if (workspace.projects[name]) {
+      throw new Error(`Project '${name}' already exists in workspace.`);
+    }
+
+    // Add project to workspace.
+    workspace.projects[name] = project;
+
+    if (!workspace.defaultProject && Object.keys(workspace.projects).length === 1) {
+      // Make the new project the default one.
+      workspace.defaultProject = name;
+    }
+
+    return updateWorkspace(workspace);
+  };
+}
+
+export function updateWorkspace(workspace: WorkspaceSchema): Rule {
+  return originalUpdateWorkspace(workspace);
+}
+
+export const configPath = originalConfigPath;
+
+export function getConfig(host: Tree): CliConfig {
+  return originalGetConfig(host);
+}
+
+export function getAppFromConfig(config: CliConfig, appIndexOrName: string): AppConfig | null {
+  return originalGetAppFromConfig(config, appIndexOrName);
+}
 
 // TODO: Remove after finishing the migration completely
-export type WorkspaceSchema = experimental.workspace.WorkspaceSchema;
-export type WorkspaceProject = experimental.workspace.WorkspaceProject;
+// export type WorkspaceSchema = experimental.workspace.WorkspaceSchema;
+// export type WorkspaceProject = experimental.workspace.WorkspaceProject;
