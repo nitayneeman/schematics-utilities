ed4d25921a1809ce1c01d450b7ba17120064b000
nitayneeman <nitayneeman@users.noreply.github.com>
2019-10-05 14:42:39 +0300

    fix: transpilation error

--- src/angular/project-targets.ts
+++ src/angular/project-targets.ts
@@ -1,19 +1,24 @@
 import { SchematicsException, Tree } from '@angular-devkit/schematics';
-import {
-  getProjectTargets as originalGetProjectTargets,
-  targetBuildNotFoundError as originalTargetBuildNotFoundError
-} from '@schematics/angular/utility/project-targets';
+import { targetBuildNotFoundError as originalTargetBuildNotFoundError } from '@schematics/angular/utility/project-targets';
 
+import { isWorkspaceProject, getProject } from './project';
 import { WorkspaceProject, WorkspaceSchema, WorkspaceTargets } from './workspace-models';
 
+// TODO: Migrate to use the original source
 export function getProjectTargets(project: WorkspaceProject): WorkspaceTargets;
 export function getProjectTargets(workspaceOrHost: WorkspaceSchema | Tree, projectName: string): WorkspaceTargets;
 export function getProjectTargets(
   projectOrHost: WorkspaceProject | Tree | WorkspaceSchema,
   projectName = ''
 ): WorkspaceTargets {
-  // TODO: fix the type error
-  return originalGetProjectTargets(<any>projectOrHost, projectName);
+  const project = isWorkspaceProject(projectOrHost) ? projectOrHost : getProject(projectOrHost, projectName);
+
+  const projectTargets = project.targets || project.architect;
+  if (!projectTargets) {
+    throw new Error('Project target not found.');
+  }
+
+  return projectTargets;
 }
 
 export function targetBuildNotFoundError(): SchematicsException {
