75ec46b4765470b55243b1c19d298d42339c85b9
nitayneeman <nitayneeman@users.noreply.github.com>
2020-01-03 21:23:51 +0200

    refactor(html-head-element): clone original code

--- src/cdk/index.ts
+++ src/cdk/index.ts
@@ -1,6 +1,7 @@
 export * from './ast';
 export * from './build-component';
 export * from './get-project';
+export * from './html-head-element';
 export * from './project-main-file';
 export * from './project-targets';
 export * from './version-agnostic-typescript';
